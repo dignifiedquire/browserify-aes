ae549bbf2f86b1697f2b917d457d809f90b24f68
dignifiedquire <dignifiedquire@gmail.com>
2018-02-20 01:25:34 +0100

    move to uint32arrays

--- aes.js
+++ aes.js
@@ -6,10 +6,8 @@
 var Buffer = require('safe-buffer').Buffer
 
 function asUInt32Array (buf) {
-  if (!Buffer.isBuffer(buf)) buf = Buffer.from(buf)
-
   var len = (buf.length / 4) | 0
-  var out = new Array(len)
+  var out = new Uint32Array(len)
 
   for (var i = 0; i < len; i++) {
     out[i] = buf.readUInt32BE(i * 4)
@@ -18,53 +16,75 @@ function asUInt32Array (buf) {
   return out
 }
 
-function scrubVec (v) {
-  for (var i = 0; i < v.length; v++) {
-    v[i] = 0
-  }
-}
-
-function cryptBlock (M, keySchedule, SUB_MIX, SBOX, nRounds) {
-  var SUB_MIX0 = SUB_MIX[0]
-  var SUB_MIX1 = SUB_MIX[1]
-  var SUB_MIX2 = SUB_MIX[2]
-  var SUB_MIX3 = SUB_MIX[3]
-
+function cryptBlock (M, keySchedule, SUB_MIX0, SUB_MIX1, SUB_MIX2, SUB_MIX3, SBOX, nRounds, t) {
   var s0 = M[0] ^ keySchedule[0]
   var s1 = M[1] ^ keySchedule[1]
   var s2 = M[2] ^ keySchedule[2]
   var s3 = M[3] ^ keySchedule[3]
-  var t0, t1, t2, t3
+  var t0, t1, t2, t3 = 0
   var ksRow = 4
+  var round = 1
 
-  for (var round = 1; round < nRounds; round++) {
-    t0 = SUB_MIX0[s0 >>> 24] ^ SUB_MIX1[(s1 >>> 16) & 0xff] ^ SUB_MIX2[(s2 >>> 8) & 0xff] ^ SUB_MIX3[s3 & 0xff] ^ keySchedule[ksRow++]
-    t1 = SUB_MIX0[s1 >>> 24] ^ SUB_MIX1[(s2 >>> 16) & 0xff] ^ SUB_MIX2[(s3 >>> 8) & 0xff] ^ SUB_MIX3[s0 & 0xff] ^ keySchedule[ksRow++]
-    t2 = SUB_MIX0[s2 >>> 24] ^ SUB_MIX1[(s3 >>> 16) & 0xff] ^ SUB_MIX2[(s0 >>> 8) & 0xff] ^ SUB_MIX3[s1 & 0xff] ^ keySchedule[ksRow++]
-    t3 = SUB_MIX0[s3 >>> 24] ^ SUB_MIX1[(s0 >>> 16) & 0xff] ^ SUB_MIX2[(s1 >>> 8) & 0xff] ^ SUB_MIX3[s2 & 0xff] ^ keySchedule[ksRow++]
+  for (; round < nRounds; round++) {
+    t0 = SUB_MIX0[s0 >>> 24] ^
+      SUB_MIX1[(s1 >>> 16) & 0xff] ^
+      SUB_MIX2[(s2 >>> 8) & 0xff] ^
+      SUB_MIX3[s3 & 0xff] ^
+      keySchedule[ksRow]
+    t1 = SUB_MIX0[s1 >>> 24] ^
+      SUB_MIX1[(s2 >>> 16) & 0xff] ^
+      SUB_MIX2[(s3 >>> 8) & 0xff] ^
+      SUB_MIX3[s0 & 0xff] ^
+      keySchedule[ksRow+1]
+    t2 = SUB_MIX0[s2 >>> 24] ^
+      SUB_MIX1[(s3 >>> 16) & 0xff] ^
+      SUB_MIX2[(s0 >>> 8) & 0xff] ^
+      SUB_MIX3[s1 & 0xff] ^
+      keySchedule[ksRow+2]
+    t3 = SUB_MIX0[s3 >>> 24] ^
+      SUB_MIX1[(s0 >>> 16) & 0xff] ^
+      SUB_MIX2[(s1 >>> 8) & 0xff] ^
+      SUB_MIX3[s2 & 0xff] ^
+      keySchedule[ksRow+3]
     s0 = t0
     s1 = t1
     s2 = t2
     s3 = t3
+    ksRow += 4
   }
 
-  t0 = ((SBOX[s0 >>> 24] << 24) | (SBOX[(s1 >>> 16) & 0xff] << 16) | (SBOX[(s2 >>> 8) & 0xff] << 8) | SBOX[s3 & 0xff]) ^ keySchedule[ksRow++]
-  t1 = ((SBOX[s1 >>> 24] << 24) | (SBOX[(s2 >>> 16) & 0xff] << 16) | (SBOX[(s3 >>> 8) & 0xff] << 8) | SBOX[s0 & 0xff]) ^ keySchedule[ksRow++]
-  t2 = ((SBOX[s2 >>> 24] << 24) | (SBOX[(s3 >>> 16) & 0xff] << 16) | (SBOX[(s0 >>> 8) & 0xff] << 8) | SBOX[s1 & 0xff]) ^ keySchedule[ksRow++]
-  t3 = ((SBOX[s3 >>> 24] << 24) | (SBOX[(s0 >>> 16) & 0xff] << 16) | (SBOX[(s1 >>> 8) & 0xff] << 8) | SBOX[s2 & 0xff]) ^ keySchedule[ksRow++]
-  t0 = t0 >>> 0
-  t1 = t1 >>> 0
-  t2 = t2 >>> 0
-  t3 = t3 >>> 0
+  t0 = ((SBOX[s0 >>> 24] << 24) |
+        (SBOX[(s1 >>> 16) & 0xff] << 16) |
+        (SBOX[(s2 >>> 8) & 0xff] << 8) |
+        SBOX[s3 & 0xff]) ^
+    keySchedule[ksRow++]
+  t1 = ((SBOX[s1 >>> 24] << 24) |
+        (SBOX[(s2 >>> 16) & 0xff] << 16) |
+        (SBOX[(s3 >>> 8) & 0xff] << 8) |
+        SBOX[s0 & 0xff]) ^
+    keySchedule[ksRow++]
+  t2 = ((SBOX[s2 >>> 24] << 24) |
+        (SBOX[(s3 >>> 16) & 0xff] << 16) |
+        (SBOX[(s0 >>> 8) & 0xff] << 8) |
+        SBOX[s1 & 0xff]) ^
+    keySchedule[ksRow++]
+  t3 = ((SBOX[s3 >>> 24] << 24) |
+        (SBOX[(s0 >>> 16) & 0xff] << 16) |
+        (SBOX[(s1 >>> 8) & 0xff] << 8) |
+        SBOX[s2 & 0xff]) ^
+    keySchedule[ksRow++]
 
-  return [t0, t1, t2, t3]
+  t[0] = t0
+  t[1] = t1
+  t[2] = t2
+  t[3] = t3
 }
 
 // AES constants
-var RCON = [0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36]
+var RCON = new Uint32Array([0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36])
 var G = (function () {
   // Compute double table
-  var d = new Array(256)
+  var d = new Uint32Array(256)
   for (var j = 0; j < 256; j++) {
     if (j < 128) {
       d[j] = j << 1
@@ -73,10 +93,10 @@ var G = (function () {
     }
   }
 
-  var SBOX = []
-  var INV_SBOX = []
-  var SUB_MIX = [[], [], [], []]
-  var INV_SUB_MIX = [[], [], [], []]
+  var SBOX = new Uint32Array(256)
+  var INV_SBOX = new Uint32Array(256)
+  var SUB_MIX = [new Uint32Array(256), new Uint32Array(256), new Uint32Array(256), new Uint32Array(256)]
+  var INV_SUB_MIX = [new Uint32Array(256), new Uint32Array(256), new Uint32Array(256), new Uint32Array(256)]
 
   // Walk GF(2^8)
   var x = 0
@@ -118,8 +138,14 @@ var G = (function () {
   return {
     SBOX: SBOX,
     INV_SBOX: INV_SBOX,
-    SUB_MIX: SUB_MIX,
-    INV_SUB_MIX: INV_SUB_MIX
+    SUB_MIX0: SUB_MIX[0],
+    SUB_MIX1: SUB_MIX[1],
+    SUB_MIX2: SUB_MIX[2],
+    SUB_MIX3: SUB_MIX[3],
+    INV_SUB_MIX0: INV_SUB_MIX[0],
+    INV_SUB_MIX1: INV_SUB_MIX[1],
+    INV_SUB_MIX2: INV_SUB_MIX[2],
+    INV_SUB_MIX3: INV_SUB_MIX[3]
   }
 })()
 
@@ -138,15 +164,21 @@ AES.prototype._reset = function () {
   var nRounds = keySize + 6
   var ksRows = (nRounds + 1) * 4
 
-  var keySchedule = []
+  this._nRounds = nRounds
+  if (!this._keySchedule) {
+    this._keySchedule = new Uint32Array(ksRows)
+    this._invKeySchedule = new Uint32Array(ksRows)
+  }
+
   for (var k = 0; k < keySize; k++) {
-    keySchedule[k] = keyWords[k]
+    this._keySchedule[k] = keyWords[k]
   }
 
   for (k = keySize; k < ksRows; k++) {
-    var t = keySchedule[k - 1]
+    var t = this._keySchedule[k - 1]
 
-    if (k % keySize === 0) {
+    var m = k % keySize
+    if (m === 0) {
       t = (t << 8) | (t >>> 24)
       t =
         (G.SBOX[t >>> 24] << 24) |
@@ -155,7 +187,7 @@ AES.prototype._reset = function () {
         (G.SBOX[t & 0xff])
 
       t ^= RCON[(k / keySize) | 0] << 24
-    } else if (keySize > 6 && k % keySize === 4) {
+    } else if (keySize > 6 && m === 4) {
       t =
         (G.SBOX[t >>> 24] << 24) |
         (G.SBOX[(t >>> 16) & 0xff] << 16) |
@@ -163,37 +195,33 @@ AES.prototype._reset = function () {
         (G.SBOX[t & 0xff])
     }
 
-    keySchedule[k] = keySchedule[k - keySize] ^ t
+    this._keySchedule[k] = this._keySchedule[k - keySize] ^ t
   }
 
-  var invKeySchedule = []
   for (var ik = 0; ik < ksRows; ik++) {
     var ksR = ksRows - ik
-    var tt = keySchedule[ksR - (ik % 4 ? 0 : 4)]
+    var tt = this._keySchedule[ksR - (ik % 4 ? 0 : 4)]
 
     if (ik < 4 || ksR <= 4) {
-      invKeySchedule[ik] = tt
+      this._invKeySchedule[ik] = tt
     } else {
-      invKeySchedule[ik] =
-        G.INV_SUB_MIX[0][G.SBOX[tt >>> 24]] ^
-        G.INV_SUB_MIX[1][G.SBOX[(tt >>> 16) & 0xff]] ^
-        G.INV_SUB_MIX[2][G.SBOX[(tt >>> 8) & 0xff]] ^
-        G.INV_SUB_MIX[3][G.SBOX[tt & 0xff]]
+      this._invKeySchedule[ik] =
+        G.INV_SUB_MIX0[G.SBOX[tt >>> 24]] ^
+        G.INV_SUB_MIX1[G.SBOX[(tt >>> 16) & 0xff]] ^
+        G.INV_SUB_MIX2[G.SBOX[(tt >>> 8) & 0xff]] ^
+        G.INV_SUB_MIX3[G.SBOX[tt & 0xff]]
     }
   }
-
-  this._nRounds = nRounds
-  this._keySchedule = keySchedule
-  this._invKeySchedule = invKeySchedule
 }
 
-AES.prototype.encryptBlockRaw = function (M) {
+AES.prototype.encryptBlockRaw = function (out, M) {
   M = asUInt32Array(M)
-  return cryptBlock(M, this._keySchedule, G.SUB_MIX, G.SBOX, this._nRounds)
+  return cryptBlock(M, this._keySchedule, G.SUB_MIX0, G.SUB_MIX1, G.SUB_MIX2, G.SUB_MIX3, G.SBOX, this._nRounds, out)
 }
 
 AES.prototype.encryptBlock = function (M) {
-  var out = this.encryptBlockRaw(M)
+  var out = new Uint32Array(4)
+  this.encryptBlockRaw(out, M)
   var buf = Buffer.allocUnsafe(16)
   buf.writeUInt32BE(out[0], 0)
   buf.writeUInt32BE(out[1], 4)
@@ -210,7 +238,8 @@ AES.prototype.decryptBlock = function (M) {
   M[1] = M[3]
   M[3] = m1
 
-  var out = cryptBlock(M, this._invKeySchedule, G.INV_SUB_MIX, G.INV_SBOX, this._nRounds)
+  var out = new Uint32Array(4)
+  cryptBlock(M, this._invKeySchedule, G.INV_SUB_MIX0, G.INV_SUB_MIX1, G.INV_SUB_MIX2, G.INV_SUB_MIX3, G.INV_SBOX, this._nRounds, out)
   var buf = Buffer.allocUnsafe(16)
   buf.writeUInt32BE(out[0], 0)
   buf.writeUInt32BE(out[3], 4)
@@ -220,9 +249,9 @@ AES.prototype.decryptBlock = function (M) {
 }
 
 AES.prototype.scrub = function () {
-  scrubVec(this._keySchedule)
-  scrubVec(this._invKeySchedule)
-  scrubVec(this._key)
+  this._keySchedule.fill(0)
+  this._invKeySchedule.fill(0)
+  this._key.fill(0)
 }
 
 module.exports.AES = AES
--- modes/ctr.js
+++ modes/ctr.js
@@ -1,30 +1,42 @@
-var xor = require('buffer-xor')
 var Buffer = require('safe-buffer').Buffer
 var incr32 = require('../incr32')
 
-function getBlock (self) {
-  var out = self._cipher.encryptBlockRaw(self._prev)
+function getBlock (out, self) {
+  self._cipher.encryptBlockRaw(out, self._prev)
   incr32(self._prev)
-  return out
 }
 
 var blockSize = 16
+var out = new Uint32Array(4)
+
 exports.encrypt = function (self, chunk) {
-  var chunkNum = Math.ceil(chunk.length / blockSize)
+  var len = chunk.length
+  var chunkNum = Math.ceil(len / blockSize)
   var start = self._cache.length
+
   self._cache = Buffer.concat([
     self._cache,
     Buffer.allocUnsafe(chunkNum * blockSize)
   ])
-  for (var i = 0; i < chunkNum; i++) {
-    var out = getBlock(self)
-    var offset = start + i * blockSize
+
+  var offset, i
+  for (i = 0; i < chunkNum; i++) {
+    getBlock(out, self)
+    offset = start + i * blockSize
     self._cache.writeUInt32BE(out[0], offset + 0)
     self._cache.writeUInt32BE(out[1], offset + 4)
     self._cache.writeUInt32BE(out[2], offset + 8)
     self._cache.writeUInt32BE(out[3], offset + 12)
   }
-  var pad = self._cache.slice(0, chunk.length)
-  self._cache = self._cache.slice(chunk.length)
-  return xor(chunk, pad)
+
+  // xor
+  for (i = 0; i < len; ++i) {
+    self._cache[i] = chunk[i] ^ self._cache[i]
+  }
+
+  var result = self._cache.slice(0, len)
+  // update cache
+  self._cache = self._cache.slice(len)
+
+  return result
 }
